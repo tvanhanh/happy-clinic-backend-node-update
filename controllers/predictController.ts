4f3f32d45fc61656e0e8335371cb3a95ce1989a5
tvhanh <hanhvan2502@gmail.com>
2026-05-02 15:19:33 +0700

    log predict

--- controllers/predictController.ts
+++ controllers/predictController.ts
@@ -5,14 +5,27 @@ import { Request, Response } from 'express';
 
 export const predictDiabetes = async (req: Request, res: Response) => {
   try {
-    const userInput = req.body; 
-    const flaskRes = await axios.post('https://happyclinic-ai-backend-update.onrender.com/api/predict', userInput);
+    const userInput = req.body;
 
-     res.status(200).json(flaskRes.data);
-     return;
+    console.log("INPUT:", userInput);
+
+    const flaskRes = await axios.post(
+      'https://happyclinic-ai-backend-update.onrender.com/api/predict',
+      userInput
+    );
+
+    console.log("FLASK RESPONSE:", flaskRes.data);
+
+    return res.status(200).json(flaskRes.data);
+
   } catch (err: any) {
-    console.error("Flask error:", err?.response?.data || err.message);
-     res.status(500).json({ error: 'AI prediction failed', details: err?.response?.data || err.message });
-     return;
+    console.log("STATUS:", err?.response?.status);
+    console.log("DATA:", err?.response?.data);
+    console.log("MESSAGE:", err.message);
+
+    return res.status(500).json({
+      error: 'AI prediction failed',
+      debug: err?.response?.data || err.message
+    });
   }
-}
+};
